596de31a63cc50eee3d52a4d7af238198a3a0828
AdanSerrano <105513344+AdanSerrano@users.noreply.github.com>
2022-05-13 23:29:58 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,83 +1,34 @@
-package com.yasi;
-import java.util.Scanner;
-public class Main {
+package SumRecur;
+
+public class SumaRecursividad {
+
     public static void main(String[] args) {
-        Operaciones obj = new Operaciones();
-        Scanner reader = new Scanner(System.in);
-        int valor1 = 0;
-        int valor2 = 0;
-        int CuboNo;
-        System.out.println("Introduzca el primer valor: ");
-        valor1 = Integer.parseInt(reader.nextLine());
-        System.out.println("Introduzca el segundo valor: ");
-        valor2 = Integer.parseInt(reader.nextLine());
-        obj.netValor1(valor1);
-        obj.netValor2(valor2);
-        System.out.println("Primer operando"+" es" + ":   " + obj.charVAlor1());
-        System.out.println("Segundo operando"+ "es" +":   " + obj.charVAlor2());
-        System.out.println(obj.charVAlor1() + " + " + obj.charVAlor2() + "   " + "Sumando valores: "  +   obj.SumarNo());
-        System.out.println( obj.charVAlor1() + " - " + obj.charVAlor2() + "   " + "Restando valores: " +  obj.RestarNo());
-        System.out.println(obj.charVAlor1() + " * " + obj.charVAlor2() + "   " + "Multiplicando valores: "  + obj.MultiplicarNo());
-        System.out.println(obj.charVAlor1() + " elevado al cubo " + " : " + obj.CuboNo());
-        System.out.println("El Residuo es: " + obj.Residuo());
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Cua:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        ntos numeros quiere ingresar?");
+        int arr[] = new int[sc.nextInt()];
+
+        System.out.println("Cargar los Valores
+        System.out.println(sumarValores(arr));
+    }
+
+
+        }else{
+
+            public static int sumarValores(int array[]) {
+                int tam = array.length;
+                int rta;
+                if (tam == 0){
+                    return 1;
+            rta = (array[tam]) +  sumarValores(array[tam-1]);;
+
+        }
+        return rta;
     }
 }
-class Operaciones {
-    public int d;
-    public int b;
-
-    public void netValor1(int valor) {
-        d = valor;
-    }
-    public void netValor2(int valor) {
-        b = valor;
-    }
-    public Integer charVAlor1() {
-        return this.d;
-    }
-    public Integer charVAlor2() {
-        return this.b;
-    }
-    public Integer SumarNo() {
-        return this.d + this.b;
-    }
-    public Integer RestarNo() {
-        return this.d - this.b;
-    }
-    public Integer MultiplicarNo() {
-        return this.d * this.b;
-    }
-    public Integer Residuo() {
-        return this.d % this.b;
-    }
-    public Double CuboNo() {
-        return Math.pow(this.d, 3);
-    }
-    public Integer Multiplicar3N(Integer num) {
-        return this.d + this.b * num;
-    }
-    public Integer Multiplicar4N(Integer num1, Integer num2) {
-        return this.d + this.b * num1 * num2;
-    }
-    public Double RaizCuadradaN() {
-        return Math.sqrt(this.d);
-    }
-    public Double CuadradoDeN() {
-        return Math.pow(this.d, 2);
-    }
-    public Boolean EsParN() {
-        if (this.d % 2 == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public Boolean EsMultiploN() {
-        if (this.d % this.b == 0) {
-            return true;
-        } else {
-            return false;
-        }
-
-    }
 }
